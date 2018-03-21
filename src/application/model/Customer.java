0c11dfde63179e63d80f905184e7b3ea93c32fec
kekwan <kenny.kwan9@gmail.com>
2018-03-21 14:53:46 -0700

    Constructor for customer

--- src/application/model/Customer.java
+++ src/application/model/Customer.java
@@ -2,9 +2,20 @@ package application.model;
 
 public class Customer {
 
+    private String custID;
     private String name;
+    private String phoneNum;
     private String emailAddress;
     private String homeAddress;
-    private String something;
+    private String DOB;
+
+    public Customer (String custID, String name, String phoneNum, String email, String addr, String DOB) {
+        this.custID = custID;
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.emailAddress = email;
+        this.homeAddress = addr;
+        this.DOB = DOB;
+    }
 
 }
