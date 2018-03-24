0d029423e651e866315ccab7f4fa1098ab4908d7
Benny Chun <bennychun97@gmail.com>
2018-03-24 00:21:43 -0700

    Did gui stuff for search, reserve and add data

--- src/application/view/AddDataScreenController.java
+++ src/application/view/AddDataScreenController.java
@@ -1,8 +1,16 @@
 package application.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 
 public class AddDataScreenController extends AbstractController {
+
+    @FXML private GridPane customerGrid;
+    @FXML private GridPane concertGrid;
+    @FXML private GridPane venueGrid;
+    @FXML private GridPane ticketGrid;
+    @FXML private GridPane bandGrid;
+    @FXML private GridPane artistGrid;
 
     @FXML
     private void handleBack(){
--- src/application/view/SearchScreenController.java
+++ src/application/view/SearchScreenController.java
@@ -1,8 +1,15 @@
 package application.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 
 public class SearchScreenController extends AbstractController{
+
+    @FXML private TableView concertTable;
+    @FXML private TableView bandTable;
+    @FXML private TableView artistTable;
+    @FXML private TableView venueTable;
+
 
     @FXML
     private void handleBack(){
