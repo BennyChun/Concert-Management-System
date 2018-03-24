99ba950a63d738af1cddbc5eba8bb2b6551981f1
Benny Chun <bennychun97@gmail.com>
2018-03-24 01:52:31 -0700

    buttons linked for search started linking reserve

--- src/application/view/AddDataScreenController.java
+++ src/application/view/AddDataScreenController.java
@@ -1,16 +1,116 @@
 package application.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class AddDataScreenController extends AbstractController {
 
+//====================================================================================================
     @FXML private GridPane customerGrid;
+    @FXML private TextField customerNameField;
+    @FXML private TextField customerIDField;
+    @FXML private TextField addressField;
+    @FXML private TextField phoneNumberField;
+    @FXML private TextField emailField;
+    @FXML private TextField dateOfBirthField;
+
+//====================================================================================================
     @FXML private GridPane concertGrid;
+    @FXML private TextField customerNameField;
+    @FXML private TextField customerIDField;
+    @FXML private TextField addressField;
+    @FXML private TextField phoneNumberField;
+    @FXML private TextField emailField;
+    @FXML private TextField dateOfBirthField;
+//====================================================================================================
     @FXML private GridPane venueGrid;
+
+//====================================================================================================
     @FXML private GridPane ticketGrid;
+    @FXML private TextField tickedIDField;
+    @FXML private TextField seatNumberField;
+    @FXML private TextField dateOfBirthField;
+    @FXML private TextField customerNameField;
+    @FXML private TextField customerIDField;
+    @FXML private TextField addressField;
+    @FXML private TextField phoneNumberField;
+    @FXML private TextField emailField;
+    @FXML private TextField dateOfBirthField;
+
+//====================================================================================================
     @FXML private GridPane bandGrid;
+    @FXML private TextField stageNameField;
+    @FXML private TextField customerIDField;
+    @FXML private TextField formationDateField;
+
+//====================================================================================================
     @FXML private GridPane artistGrid;
+
+
+
+    @FXML
+    private void handleCustomer(){
+        customerGrid.setVisible(true);
+        concertGrid.setVisible(false);
+        venueGrid.setVisible(false);
+        ticketGrid.setVisible(false);
+        bandGrid.setVisible(false);
+        artistGrid.setVisible(false);
+
+    }
+
+    @FXML
+    private void handleConcert(){
+        customerGrid.setVisible(false);
+        concertGrid.setVisible(true);
+        venueGrid.setVisible(false);
+        ticketGrid.setVisible(false);
+        bandGrid.setVisible(false);
+        artistGrid.setVisible(false);
+    }
+
+    @FXML
+    private void handleVenue(){
+        customerGrid.setVisible(false);
+        concertGrid.setVisible(false);
+        venueGrid.setVisible(true);
+        ticketGrid.setVisible(false);
+        bandGrid.setVisible(false);
+        artistGrid.setVisible(false);
+
+    }
+
+    @FXML
+    private void handleTicket(){
+        customerGrid.setVisible(false);
+        concertGrid.setVisible(false);
+        venueGrid.setVisible(false);
+        ticketGrid.setVisible(true);
+        bandGrid.setVisible(false);
+        artistGrid.setVisible(false);
+    }
+
+    @FXML
+    private void handleBand(){
+        customerGrid.setVisible(false);
+        concertGrid.setVisible(false);
+        venueGrid.setVisible(false);
+        ticketGrid.setVisible(false);
+        bandGrid.setVisible(true);
+        artistGrid.setVisible(false);
+
+    }
+
+    @FXML
+    private void handleArtist(){
+        customerGrid.setVisible(false);
+        concertGrid.setVisible(false);
+        venueGrid.setVisible(false);
+        ticketGrid.setVisible(false);
+        bandGrid.setVisible(false);
+        artistGrid.setVisible(true);
+    }
 
     @FXML
     private void handleBack(){
--- src/application/view/SearchScreenController.java
+++ src/application/view/SearchScreenController.java
@@ -1,15 +1,73 @@
 package application.view;
 
+import application.model.Concert;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class SearchScreenController extends AbstractController{
 
-    @FXML private TableView concertTable;
-    @FXML private TableView bandTable;
-    @FXML private TableView artistTable;
-    @FXML private TableView venueTable;
+//==========================================================================================
+    @FXML private TableView<Concert> concertTable;
+    @FXML private TableColumn concertNameColumn;
+    @FXML private TableColumn concertIDColumn;
+    @FXML private TableColumn durationColumn;
+    @FXML private TableColumn startDateColumn;
+    @FXML private TableColumn is19PlusColumn;
 
+//==========================================================================================
+    @FXML private TableView bandTable;
+    @FXML private TableColumn stageNameColumn;
+    @FXML private TableColumn genreColumn;
+    @FXML private TableColumn formationDateColumn;
+
+//==========================================================================================
+    @FXML private TableView artistTable;W
+    @FXML private TableColumn artistNameColumn;
+    @FXML private TableColumn dateOfBirthColumn;
+    @FXML private TableColumn originColumn;
+    @FXML private TableColumn artistStageNameColumn;
+
+//==========================================================================================
+    @FXML private TableView venueTable;
+    @FXML private TableColumn venueNameColumn;
+    @FXML private TableColumn capacityColumn;
+    @FXML private TableColumn streetAddresssColumn;
+    @FXML private TableColumn cityColumn;
+
+
+    @FXML
+    private void handleConcert(){
+        concertTable.setVisible(true);
+        bandTable.setVisible(false);
+        artistTable.setVisible(false);
+        venueTable.setVisible(false);
+    }
+
+    @FXML
+    private void handleVenue(){
+        venueTable.setVisible(true);
+        concertTable.setVisible(false);
+        bandTable.setVisible(false);
+        artistTable.setVisible(false);
+
+    }
+
+    @FXML
+    private void handleBand(){
+        bandTable.setVisible(true);
+        concertTable.setVisible(false);
+        artistTable.setVisible(false);
+        venueTable.setVisible(false);
+    }
+
+    @FXML
+    private void handleArtist(){
+        artistTable.setVisible(true);
+        concertTable.setVisible(false);
+        bandTable.setVisible(false);
+        venueTable.setVisible(false);
+    }
 
     @FXML
     private void handleBack(){
