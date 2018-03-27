47e0c8168a4a0162279161f274b170fa0ea1f92b
Benny Chun <bennychun97@gmail.com>
2018-03-26 18:12:21 -0700

    register customer done, typechecking done, and valid ID checking done

--- src/application/view/CustomerLoginController.java
+++ src/application/view/CustomerLoginController.java
@@ -1,11 +1,17 @@
 package application.view;
 
 import application.User;
+import application.database.DatabaseManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 import java.util.Random;
 
 public class CustomerLoginController extends AbstractController {
@@ -31,25 +37,57 @@ public class CustomerLoginController extends AbstractController {
     private void handleContinue(){
         User.getInstance().initManager(false);
         if (!isNewCust) {
-            System.out.println(customerNumberField.getText());
-            _mainApp.initOptionSelect(customerNumberField.getText());
+            if(isValidUsername()) {
+                _mainApp.initOptionSelect(customerNumberField.getText());
+            } else {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning");
+                alert.setHeaderText("Not a valid customer ID");
+                alert.setContentText("Please try again with a valid customer ID or select register!");
+                alert.showAndWait();
+            }
         } else if(isNewCust){
             // TODO : Check fields and insert new tuple SQL
             // need to store these details into SQL DB
-            System.out.println(nameField.getText());
-            System.out.println(addressField.getText());
-            System.out.println(phoneNumberField.getText());
-            System.out.println(emailLabel.getText());
-            System.out.println(dobField.getValue());
             String customerID = generateCustID();
-            _mainApp.initOptionSelect(customerID);
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+            alert.setHeaderText("Update Customer Details");
+            alert.setContentText("Are you sure your details are correct?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                if (isValidInput()){
+                    String newName = "'" + nameField.getText() +"'";
+                    String newAddress = "'" + addressField.getText() + "'";
+                    String newEmail = "'" + emailField.getText()+ "'";
+                    String newPhoneNumber = "'" + phoneNumberField.getText() +"'";
+                    String dob = dobField.getValue().toString();
+                    dob = dob.replaceAll("-","/");
+                    String newDOB = "'" + dob + "'";
+                    String sql = "insert into customers values " + "('" + customerID + "', " + newName + ", " + newAddress + ", "
+                                    + newDOB + ", " + newPhoneNumber + ", " + newEmail + ")";
+                    System.out.println(sql);
+                    DatabaseManager.sendUpdate(sql);
+                    Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+                    successAlert.setTitle("Information Dialog");
+                    successAlert.setHeaderText("Upate Customer Details");
+                    successAlert.setContentText("Register successful, here is your new customer ID: " + customerID);
+                    successAlert.showAndWait();
+                    _mainApp.initOptionSelect(customerID);
+                }
+            } else {
+                // ... user chose CANCEL or closed the dialog
+            }
+
 
         }
     }
 
     private String generateCustID() {
         String custID = "";
-        for(int i=0; i < 10; i++) {
+        for(int i=0; i < 6; i++) {
            Random rnd = new Random();
            custID = custID + rnd.nextInt(9);
         }
@@ -64,6 +102,50 @@ public class CustomerLoginController extends AbstractController {
         customerNumberLabel.setVisible(false);
         setRegisterFields();
 
+
+
+    }
+
+    private Boolean isValidInput(){
+        String errorMessage = "";
+
+        if (nameField.getText() == null ||(nameField).getText().length() == 0) {
+            errorMessage += "Not a valid name!\n";
+        }
+        if (addressField.getText() != null || addressField.getText().length() != 0) {
+            if(addressField.getText().length() > 40){
+                errorMessage += "Not a valid address!\n";
+            }
+        }
+        if (emailField.getText() == null || emailField.getText().length() == 0) {
+            errorMessage += "Not a valid e-mail address!\n";
+        }
+        System.out.println(phoneNumberField.getText().length());
+        if(phoneNumberField.getText() != null || phoneNumberField.getText().length() != 0) {
+            if (phoneNumberField.getText().length() != 12) {
+                errorMessage += "Not a valid phone number!\n";
+            }
+        }
+        //TODO regex check on DOB
+        String dob = dobField.getValue().toString();
+        dob = dob.replaceAll("-", "/");
+        if (dob == null || dob.length() == 0 ) {
+            errorMessage += "Not a valid date of birth!\n";
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+
+            return false;
+        }
     }
 
     @FXML
@@ -82,5 +164,19 @@ public class CustomerLoginController extends AbstractController {
         emailLabel.setVisible(true);
         dobField.setVisible(true);
         dobLabel.setVisible(true);
+    }
+
+    private boolean isValidUsername () {
+        String sql = "select cust_id from customers where cust_id = " + "'" + customerNumberField.getText() + "'";
+        ResultSet rs = DatabaseManager.sendQuery(sql);
+        try {
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        DatabaseManager.closeStatement();
+        return false;
     }
 }
